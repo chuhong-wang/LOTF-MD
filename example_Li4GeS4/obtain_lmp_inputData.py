ae6056de76fd7c5ff870d085e504752b455e4bb6
Chuhong Wang <chuhong.wang@bytedance.com>
2022-10-16 22:57:36 -0700

    MD example

---
+++ example_Li4GeS4/obtain_lmp_inputData.py
@@ -0,0 +1,15 @@
+from pymatgen.io.lammps.data import LammpsData,structure_2_lmpdata
+from pymatgen.core.structure import Structure
+
+import shutil
+import sys
+import pymatgen as mg
+
+from pymatgen.ext.matproj import MPRester
+from pymatgen.io.vasp.sets import MPRelaxSet
+
+structure=Structure.from_file("POSCAR")
+
+l=structure_2_lmpdata(structure,atom_style="atomic")
+l.write_file('start.inp')
+
